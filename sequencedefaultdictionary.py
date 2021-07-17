2c078829743bc3b7d12d85c0aa6bc5e22791955a
Anthony Curtis Adler <anthony.adler@gmail.com>
2021-07-18 08:53:50 +0900

    Add files via upload
    when entering an automatically queried index or float sequence key, you can revise the only the last digits of the index by beginning with a PERIOD.
    For example.
    OLD INDEX 3.4.5.6.7
    ENTER: .2.2
    NEW INDEX: 3.4.5.2.2
    
    OLD FLOAT 3.4134
    ENTER .566
    NEW FLOAT 3.566
    
    
    To enter a floating point value without an integer part, you 0 before the PERIOD.

--- sequencedefaultdictionary.py
+++ sequencedefaultdictionary.py
@@ -34,6 +34,16 @@ class SequenceDefaultDictionary:
                     return False
           return False
 
+     def is_index(self,keyword):
+          if keyword in self.defaultdictionary:
+
+               val = self.defaultdictionary[keyword]
+               if val[0] !='.' and val[-1] != '.' and '.' in val and val.replace('.','').isnumeric():
+                    return True
+                    
+          return False
+
+
 
      def show (self,phrase):
 
@@ -47,7 +57,8 @@ class SequenceDefaultDictionary:
           """either add to value, subtracts from value, or replaces it.
           Addition and subtraction for repeated plus or minus signs, according to legnth"""
 
-          
+          if value == '/':
+               return value
           keyword = split(phrase)
           if value == ' ' and keyword in self.defaultdictionary:
                value = self.defaultdictionary[keyword]
@@ -56,7 +67,7 @@ class SequenceDefaultDictionary:
           else:
                if value[0] == '=':
                     value = value[1:]
-               elif  self.is_float(keyword):
+               elif  self.is_float(keyword) and value and not value[0]=='.':
                     if isonly(value,'+') and self.is_float(keyword):
                          value = str(float(self.defaultdictionary[keyword])+len(value))
                     elif isonly(value,'-') and self.is_float(keyword):
@@ -73,6 +84,13 @@ class SequenceDefaultDictionary:
                               pass
                     if self.is_numeric(keyword):
                          value = str(int(float(value)))
+               elif self.is_index(keyword):
+                    if len(value) > 1 and value[0]=='.' and value.count('.')<=self.defaultdictionary[keyword].count('.'):
+                         index_values = self.defaultdictionary[keyword].split('.')
+                         new_values = value[1:].split('.')
+                         value = '.'.join(index_values[0:-len(new_values)]+new_values)
+                    
+                         
                          
                          
                
